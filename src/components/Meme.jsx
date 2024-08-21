d09434e0ac939acd8ada71e55eceb654fe7c18b8
shourya-r <rajputshourya7@gmail.com>
2024-08-21 19:31:40 +0530

    Added ability to add text using form and state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -9,6 +9,17 @@ function Meme() {
 
   const [allMemeImages, setAllMemeImages] = useState(data);
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  }
+
   function getMemeImage() {
     const memesArray = allMemeImages.data.memes;
     const randomMeme =
@@ -22,14 +33,30 @@ function Meme() {
   return (
     <main>
       <div className="form">
-        <input className="form--input" placeholder="Top text" type="text" />
-        <input className="form--input" placeholder="Bottom text" type="text" />
+        <input
+          className="form--input"
+          placeholder="Top text"
+          type="text"
+          name="topText"
+          onChange={handleChange}
+          value={meme.topText}
+        />
+        <input
+          className="form--input"
+          placeholder="Bottom text"
+          type="text"
+          name="bottomText"
+          onChange={handleChange}
+          value={meme.bottomText}
+        />
         <button onClick={getMemeImage} className="form--button">
           Get a new meme image
         </button>
       </div>
       <div className="image--container">
         <img className="main--image" src={meme.randomImage}></img>
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );
